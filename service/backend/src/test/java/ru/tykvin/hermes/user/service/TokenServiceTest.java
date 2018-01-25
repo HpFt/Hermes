f8f8f3d9df5aa5438ca3580d6735f0fbbcf4c0b2
HpFt <happyfat25@gmail.com>
2018-01-25 21:46:16 +0300

    Tests

--- service/backend/src/test/java/ru/tykvin/hermes/user/service/TokenServiceTest.java
+++ service/backend/src/test/java/ru/tykvin/hermes/user/service/TokenServiceTest.java
@@ -1,6 +1,7 @@
 package ru.tykvin.hermes.user.service;
 
 import io.jsonwebtoken.Jwts;
+import org.jooq.DSLContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 import ru.tykvin.hermes.PostgresqlEmbeddedConfiguration;
 import ru.tykvin.hermes.lib.JsonUtils;
 import ru.tykvin.hermes.model.TokenData;
+import ru.tykvin.hermes.model.User;
+import ru.tykvin.hermes.user.exception.NeedUpdateTokenException;
 
 import javax.xml.bind.DatatypeConverter;
+import java.util.UUID;
 
+import static credit.station.jooq.tables.Users.USERS;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 @Import(PostgresqlEmbeddedConfiguration.class)
@@ -26,6 +32,10 @@ public class TokenServiceTest {
 
     @Autowired
     private TokenService tokenService;
+
+    @Autowired
+    private DSLContext dslContext;
+
     @Value("${api.secret}")
     private String secret;
 
@@ -37,17 +47,35 @@ public class TokenServiceTest {
                 .parse(cipher).getBody();
         TokenData tokenData = JsonUtils.getObjectMapper().convertValue(token, TokenData.class);
         assertThat(tokenData.getUser().getIp(), equalTo("192.168.0.1"));
+        assertThat(checkUser("192.168.0.1"), notNullValue());
     }
 
     @Test
     public void createUser() {
+        User user = tokenService.createUser("192.168.0.2");
+        assertThat(checkUser("192.168.0.2"), equalTo(user));
     }
 
     @Test
-    public void validateToken() {
+    public void validToken() {
+        String cipher = tokenService.createToken("192.168.0.3");
+        tokenService.validateToken(cipher, "192.168.0.3");
+    }
+
+    @Test(expected = NeedUpdateTokenException.class)
+    public void notValidToken() {
+        String cipher = tokenService.createToken("192.168.0.4");
+        tokenService.validateToken(cipher, "192.168.0.2");
     }
 
     @Test
     public void getToken() {
+        String cipher = tokenService.createToken("192.168.0.5");
+        TokenData tokenData = tokenService.getToken(cipher);
+        assertThat(tokenData.getUser().getIp(), equalTo("192.168.0.5"));
+    }
+
+    private User checkUser(String ip) {
+        return dslContext.selectFrom(USERS).where(USERS.IP.eq(ip)).fetchOptional(r -> new User(UUID.fromString(r.getId()), r.getCreateAt(), r.getIp())).get();
     }
 }
