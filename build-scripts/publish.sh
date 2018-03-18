8399d99a05e81291027d1e3e1beba3e9f2bd0d18
happyfat <happyfat25@gmail.com>
2018-03-18 03:07:51 +0300

    Docker CI fix

--- build-scripts/publish.sh
+++ build-scripts/publish.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 [[ ! -z "$DOCKER_USERNAME" ]] && docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+cp -rf ../build/libs/Hermes.jar build-scripts/app.jar
 docker image build -t happyfat25/hermes:latest .
 docker push happyfat25/hermes:latest
